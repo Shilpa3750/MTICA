89d655103d600098386a95419d4f71624c2cbd71
Shilpa3750 <121286321+Shilpa3750@users.noreply.github.com>
2023-01-07 14:36:25 +0530

    Add files via upload

---
+++ day15program11.py
@@ -0,0 +1,12 @@
+
+def outer():
+    message='outer scope'
+    print(message)
+    def inner():
+        nonlocal message
+        message='inner scope'
+        print('inner:',message)
+      
+    inner()
+    print('outer:',message)
+outer()
