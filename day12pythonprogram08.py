08225587e866ed8d64dd5ca8f3e899a0dbb46e81
Shilpa3750 <121286321+Shilpa3750@users.noreply.github.com>
2023-01-07 14:38:26 +0530

    Add files via upload

---
+++ day12pythonprogram08.py
@@ -0,0 +1,12 @@
+set1={10,20,30,40,50}
+set2={60,70,80,90,10}
+if set1.isdisjoint(set2):
+    print("Two sets have items in common")
+else:
+    print("Two sets have items in common")
+    print(set1.intersection(set2))
+    
+set1={10,20,30,40,50}
+set2={30,40,50,60,70}
+set.symmetric_difference_update(set2)
+print(set1)
