149e43441b5718cd9015ef7db241b5d7ef137457
Shilpa3750 <121286321+Shilpa3750@users.noreply.github.com>
2023-01-07 14:37:41 +0530

    Add files via upload

---
+++ day10pythonprograms03.py
@@ -0,0 +1,16 @@
+def printme(str1,n):
+    n[0]='prasad'
+    print(str1,n)
+    return
+x=['silpa','nithya']
+printme("Wakeup",x)
+print('x:',x)
+
+def changeMe(lstn):
+
+    lstn=['prasad','manohar','manasa']
+    print(lstn)
+    return
+lst=[1,4,11,55]
+print(changeMe(lst))
+print('lst:',lst)
---
+++ day10pythonprograms04.py
@@ -0,0 +1,14 @@
+def printDetail(name,marks):
+    print("Name:",name)
+    print("Marks:",name)
+    return None
+
+
+printDetail()
+##printDetail('Manohar')
+##printDetail('Manohar',87)
+##printDetail(87,'Manohar')
+
+
+
+printDetail(marks=87,name='Manohar')
